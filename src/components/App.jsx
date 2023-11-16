b1913da4a888e26ea667fd55c13cdd611fcdc7d7
Kushnierova <Katerynakushnierova@gmail.com>
2023-11-16 14:08:25 +0100

    14:08

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
+import React, { Component } from 'react';
 import Feedback from './Feedback/Feedback';
 
-export const App = () => {
-  return (
-    <div>
-Hello
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    const options = Object.keys(this.state);
+    return (
+      <>
+        <h1>hello</h1>
+        <Feedback />
+      </>
+    );
+  }
+}
+
+export default App;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import css from './Feedback.css';
 
-const Feedback = ({ feedback }) => <ul>Feedback</ul>;
+const Feedback = ({ options, onLeaveFeedback }) => {
+  <ul className="btnList">
+    {options.map(option => (
+      <li key={option}>
+        <button type="button" onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      </li>
+    ))}
+  </ul>;
+};
 
 export default Feedback;
